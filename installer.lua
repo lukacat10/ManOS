567f7bf03370aa25a62e6db98d34953dfaa23d05
lukacat10 <lukacat100@gmail.com>
2019-12-30 01:17:23 +0200

    Installer script

---
+++ installer.lua
@@ -0,0 +1,8 @@
+shell.run("wget https://github.com/lukacat10/ManOS/blob/master/manos.lua")
+shell.run("https://github.com/lukacat10/ManOS/blob/master/manos_dir/quarry_manager.lua")
+shell.run("https://github.com/lukacat10/ManOS/blob/master/manos_dir/sharing_manager.lua")
+shell.run("https://github.com/lukacat10/ManOS/blob/master/manos_dir/sheets.lua")
+shell.run("mkdir manos_dir")
+shell.run("mv quarry_manager.lua manos_dir/quarry_manager.lua")
+shell.run("mv sharing_manager.lua manos_dir/sharing_manager.lua")
+shell.run("mv sheets.lua manos_dir/sheets.lua")
