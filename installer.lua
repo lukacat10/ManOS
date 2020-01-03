8e32c17ade64b5f7e571b68e457c5c99c942fe10
lukacat10 <lukacat100@gmail.com>
2020-01-03 16:51:03 +0200

    Added reactor control class

--- installer.lua
+++ installer.lua
@@ -1,3 +1,4 @@
+--run as startup
 if shell.getRunningProgram() ~= "startup" then
     if fs.exists("startup") then
         fs.delete("startup")
@@ -7,6 +8,7 @@ if shell.getRunningProgram() ~= "startup" then
     fs.copy(shell.getRunningProgram(), "startup")
     os.reboot()
 end
+--END
 --Deleting to update the files
 if fs.exists("installer.lua") then
     fs.delete("installer.lua")
@@ -52,11 +54,17 @@ end
 if fs.exists("sharing_manager.lua") then
     fs.delete("sharing_manager.lua")
 end
+if fs.exists("reactor_manager.lua") then
+    fs.delete("reactor_manager.lua")
+end
+
 -- END
+-- Download all the libraries and the program itself
 shell.run("wget https://raw.githubusercontent.com/lukacat10/ManOS/master/manos.lua")
 shell.run("mkdir manos_dir")
 shell.run("wget https://raw.githubusercontent.com/lukacat10/ManOS/master/manos_dir/quarry_manager.lua manos_dir/quarry_manager.lua")
 shell.run("wget https://raw.githubusercontent.com/lukacat10/ManOS/master/manos_dir/sharing_manager.lua manos_dir/sharing_manager.lua")
+shell.run("wget https://raw.githubusercontent.com/lukacat10/ManOS/master/manos_dir/reactor_manager.lua manos_dir/reactor_manager.lua")
 shell.run("wget https://raw.githubusercontent.com/lukacat10/ManOS/master/manos_dir/sheets.lua manos_dir/sheets.lua")
 shell.run("pastebin get https://pastebin.com/iwc51FGa manos_programs/open")
 shell.run("pastebin get https://pastebin.com/DNFCQ38i manos_programs/close")
@@ -65,10 +73,13 @@ shell.run("pastebin get https://pastebin.com/BvdBz8bP manos_programs/stopsharing
 if fs.exists("json") ~= true then
     shell.run("pastebin get 4nRg9CHU json")
 end
+--END
+-- Set ManOS to run at startup
 if fs.exists("startup") then
     fs.delete("startup")
 elseif fs.exists("startup.lua") then
     fs.delete("startup.lua")
 end
 fs.move("manos.lua", "startup")
+--END
 os.reboot()
